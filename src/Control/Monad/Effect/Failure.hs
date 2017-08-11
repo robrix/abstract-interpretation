5d937ce1f681e066a247c2df8d3f3348634999a0
Rob Rix <rob.rix@me.com>
2017-08-11 19:43:40 -0400

    Define a MonadFail instance.

--- src/Control/Monad/Effect/Failure.hs
+++ src/Control/Monad/Effect/Failure.hs
@@ -1,5 +1,11 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts, GADTs, TypeOperators, UndecidableInstances #-}
 module Control.Monad.Effect.Failure where
+
+import Control.Monad.Effect
+import Control.Monad.Fail
 
 data Failure a where
   Failure :: String -> Failure a
+
+instance Failure :< fs => MonadFail (Eff fs) where
+  fail = send . Failure

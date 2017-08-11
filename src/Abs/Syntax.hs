321f90030b35c812e6a7a2319dec97aec21f6d69
Rob Rix <rob.rix@me.com>
2017-08-11 19:57:46 -0400

    Add some type synonyms.

--- src/Abs/Syntax.hs
+++ src/Abs/Syntax.hs
@@ -1,6 +1,8 @@
 module Abs.Syntax where
 
 import Data.Functor.Foldable
+import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
 
 data Syntax n a
   = Var n
@@ -20,6 +22,11 @@ data Op2 = Plus | Minus | Times | DividedBy
 data Ref a = Ref
 
 delta :: Monad m => Op2 -> Int -> Int -> m Int
+
+type Environment = Map.Map String Loc
+type Loc = Int
+type Val = Int
+type Store = IntMap.IntMap Loc
 delta o = (return .) . case o of
   Plus -> (+)
   Minus -> (-)

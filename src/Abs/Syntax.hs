ee20e2f99d7b3fd8c7ec1ad413b2e3b3f965b32d
Rob Rix <rob.rix@me.com>
2017-08-11 18:30:58 -0400

    Add binary operations.

--- src/Abs/Syntax.hs
+++ src/Abs/Syntax.hs
@@ -5,9 +5,12 @@ import Data.Functor.Foldable
 data Syntax n a
   = Var n
   | Num Int
+  | Op2 Op2 a a
   | App a a
   | Lam n a
   | Rec n a
   | If0 a a a
 
 type Term = Fix (Syntax String)
+
+data Op2 = Plus | Minus | Times | DividedBy

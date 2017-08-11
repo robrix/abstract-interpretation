20f75e4f38d9d687a885b893add1bb829dd14291
Rob Rix <rob.rix@me.com>
2017-08-11 19:48:34 -0400

    Define an interpreter for Failure.

--- src/Control/Monad/Effect/Failure.hs
+++ src/Control/Monad/Effect/Failure.hs
@@ -1,11 +1,15 @@
-{-# LANGUAGE FlexibleContexts, GADTs, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, TypeOperators, UndecidableInstances #-}
 module Control.Monad.Effect.Failure where
 
 import Control.Monad.Effect
+import Control.Monad.Effect.Internal
 import Control.Monad.Fail
 
 data Failure a where
   Failure :: String -> Failure a
+
+runFailure :: Eff (Failure ': fs) a -> Eff fs (Either String a)
+runFailure = relay (pure . Right) (\(Failure s) _ -> pure (Left s))
 
 instance Failure :< fs => MonadFail (Eff fs) where
   fail = send . Failure

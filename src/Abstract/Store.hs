42a76732fef6cc4d7fc99abc6a37abf6f2efc4a5
Rob Rix <rob.rix@me.com>
2017-08-12 19:51:04 -0400

    Define an alternative set of store functions.

--- src/Abstract/Store.hs
+++ src/Abstract/Store.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE FlexibleContexts, ScopedTypeVariables, TypeOperators #-}
 module Abstract.Store where
 
+import Control.Applicative
 import Control.Monad.Effect
 import Control.Monad.Effect.State
+import Data.Foldable (asum)
 import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
+import Data.Semigroup
 
 type Store v = IntMap.IntMap v
 
@@ -20,3 +24,20 @@ alloc _ = do
 
 ext :: (State (Store a) :< fs) => Loc a -> a -> Eff fs ()
 ext (Loc loc) val = modify (IntMap.insert loc val)
+
+
+newtype Loc' a = Loc' { unLoc' :: String }
+  deriving (Eq, Ord, Show)
+
+type Store' a = Map.Map (Loc' a) [a]
+
+find' :: forall a fs. (Alternative (Eff fs), State (Store' a) :< fs) => Loc' a -> Eff fs a
+find' loc = do
+  store <- get
+  asum (return <$> ((store :: Store' a) Map.! loc))
+
+alloc' :: Alternative m => String -> m (Loc' a)
+alloc' x = pure (Loc' x)
+
+ext' :: (State (Store' a) :< fs) => Loc' a -> a -> Eff fs ()
+ext' loc val = modify (Map.insertWith (<>) loc [val])

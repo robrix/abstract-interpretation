0c5b8991c28ab08e9b5500ad579ffbe1d2e5c566
Rob Rix <rob.rix@me.com>
2017-08-11 21:04:25 -0400

    Define Eq1, Eq2, Show1, & Show2 instances for Syntax.

--- src/Abs/Syntax.hs
+++ src/Abs/Syntax.hs
@@ -9,6 +9,7 @@ import qualified Control.Monad.Effect.State as State
 import Control.Monad.Reader.Class
 import Control.Monad.State.Class
 import Data.Function ((&))
+import Data.Functor.Classes
 import Data.Functor.Foldable
 import qualified Data.IntMap as IntMap
 import qualified Data.Map as Map
@@ -101,3 +102,32 @@ instance State :< fs => MonadState Store (Eff fs) where
 instance Reader :< fs => MonadReader Environment (Eff fs) where
   ask = Reader.ask
   local = Reader.local
+
+instance Eq2 Syntax where
+  liftEq2 eqN eqA s1 s2 = case (s1, s2) of
+    (Var n1, Var n2) -> eqN n1 n2
+    (Num v1, Num v2) -> v1 == v2
+    (Op2 o1 a1 b1, Op2 o2 a2 b2) -> o1 == o2 && eqA a1 a2 && eqA b1 b2
+    (App a1 b1, App a2 b2) -> eqA a1 a2 && eqA b1 b2
+    (Lam n1 a1, Lam n2 a2) -> eqN n1 n2 && eqA a1 a2
+    (Rec n1 a1, Rec n2 a2) -> eqN n1 n2 && eqA a1 a2
+    (If0 c1 t1 e1, If0 c2 t2 e2) -> eqA c1 c2 && eqA t1 t2 && eqA e1 e2
+    _ -> False
+
+instance Eq n => Eq1 (Syntax n) where
+  liftEq = liftEq2 (==)
+
+instance Show2 Syntax where
+  liftShowsPrec2 spN _ spA _ d s = case s of
+    Var n -> showsUnaryWith spN "Var" d n
+    Num v -> showsUnaryWith showsPrec "Num" d v
+    Op2 o a b -> showsTernaryWith showsPrec spA spA "Op2" d o a b
+    App a b -> showsBinaryWith spA spA "App" d a b
+    Lam n a -> showsBinaryWith spN spA "Lam" d n a
+    Rec n a -> showsBinaryWith spN spA "Rec" d n a
+    If0 c t e -> showsTernaryWith spA spA spA "If0" d c t e
+    where showsTernaryWith :: (Int -> a -> ShowS) -> (Int -> b -> ShowS) -> (Int -> c -> ShowS) -> String -> Int -> a -> b -> c -> ShowS
+          showsTernaryWith sp1 sp2 sp3 name d x y z = showParen (d > 10) $ showString name . showChar ' ' . sp1 11 x . showChar ' ' . sp2 11 y . showChar ' ' . sp3 11 z
+
+instance Show n => Show1 (Syntax n) where
+  liftShowsPrec = liftShowsPrec2 showsPrec showList

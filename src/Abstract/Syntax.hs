b7af49c4de35952e11e3991abc03a012adfc5a69
Rob Rix <rob.rix@me.com>
2017-08-12 18:29:50 -0400

    Move all things evaluation into the eval module.

--- src/Abstract/Syntax.hs
+++ src/Abstract/Syntax.hs
@@ -1,25 +1,13 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Abstract.Syntax where
 
-import Abstract.Store
 import Abstract.Value
-import Control.Effect
-import Control.Monad.Fail
-import Control.Monad.Effect as Effect hiding (run)
-import Control.Monad.Effect.Failure
-import Control.Monad.Effect.Reader as Reader
-import Control.Monad.Effect.State as State
-import Control.Monad.Effect.Writer as Writer
 import Data.Bifunctor
-import Data.Function (fix)
 import Data.Functor.Classes
 import Data.Functor.Foldable
-import qualified Data.Map as Map
 import Data.Semigroup
 import qualified Data.Set as Set
-import GHC.Exts (IsList(..))
-import Prelude hiding (fail)
 
 data Syntax i n a
   = Var n
@@ -62,123 +50,6 @@ subexps = para $ \ s -> case s of
   Rec _ a -> Set.singleton (fst a) <> snd a
   If0 c t e -> foldMap (Set.singleton . fst) [c, t, e] <> foldMap snd [c, t, e]
   _ -> Set.empty
-
-
-type Environment i = Map.Map String (Loc (Val i))
-data Val i = I i | L (Term i, Environment i)
-  deriving (Eq, Ord, Show)
-
-
--- Evaluation
-
-eval :: forall i . AbstractValue i (Eff (Interpreter i)) => Term i -> (Either String (Val i), Store (Val i))
-eval = run @(Interpreter i) . runEval
-
-runEval :: (Interpreter i :<: fs, AbstractValue i (Eff fs)) => Term i -> Eff fs (Val i)
-runEval = fix ev
-
-ev :: (AbstractValue i (Eff fs), Interpreter i :<: fs)
-   => (Term i -> Eff fs (Val i))
-   -> Term i
-   -> Eff fs (Val i)
-ev ev term = case unfix term of
-  Num n -> return (I n)
-  Var x -> do
-    p <- ask
-    find (p Map.! x)
-  If0 c t e -> do
-    v <- ev c
-    z <- isZero v
-    ev (if z then t else e)
-  Op1 o a -> do
-    va <- ev a
-    delta1 o va
-  Op2 o a b -> do
-    va <- ev a
-    vb <- ev b
-    delta2 o va vb
-  Rec f e -> do
-    p <- ask
-    a <- alloc f
-    let p' = Map.insert f a p
-    v <- local (const p') (ev e)
-    ext a v
-    return v
-  Lam x e0 -> do
-    p <- ask
-    return (L (Fix (Lam x e0), p))
-  App e0 e1 -> do
-    (L (Fix (Lam x e2), p)) <- ev e0
-    v1 <- ev e1
-    a <- alloc x
-    ext a v1
-    local (const (Map.insert x a p)) (ev e2)
-
-
--- Tracing and reachable state analyses
-
-evalTrace :: forall i. AbstractValue i (Eff (TraceInterpreter i)) => Term i -> (Either String (Val i, Trace i []), Store (Val i))
-evalTrace = run @(TraceInterpreter i) . runTrace
-
-runTrace :: (TraceInterpreter i :<: fs, AbstractValue i (Eff fs)) => Term i -> Eff fs (Val i)
-runTrace = fix (evTell [] ev)
-
-evalReach :: forall i. (Ord i, AbstractValue i (Eff (ReachableStateInterpreter i))) => Term i -> (Either String (Val i, Trace i Set.Set), Store (Val i))
-evalReach = run @(ReachableStateInterpreter i) . runReach
-
-runReach :: (Ord i, ReachableStateInterpreter i :<: fs, AbstractValue i (Eff fs)) => Term i -> Eff fs (Val i)
-runReach = fix (evTell Set.empty ev)
-
-evTell :: forall i f fs . (TracingInterpreter i f :<: fs, IsList (Trace i f), Item (Trace i f) ~ TraceEntry i)
-       => f ()
-       -> ((Term i -> Eff fs (Val i)) -> Term i -> Eff fs (Val i))
-       -> (Term i -> Eff fs (Val i))
-       -> Term i
-       -> Eff fs (Val i)
-evTell _ ev0 ev e = do
-  env <- ask
-  store <- get
-  tell (fromList [(e, env, store)] :: Trace i f)
-  ev0 ev e
-
-
--- Dead code analysis
-
-evalDead :: forall i. (Ord i, AbstractValue i (Eff (DeadCodeInterpreter i))) => Term i -> (Either String (Val i, Set.Set (Term i)), Store (Val i))
-evalDead = run @(DeadCodeInterpreter i) . runDead
-
-runDead :: (Ord i, DeadCodeInterpreter i :<: fs, AbstractValue i (Eff fs)) => Term i -> Eff fs (Val i)
-runDead e0 = do
-  put (subexps e0)
-  fix (evDead ev) e0
-
-evDead :: (Ord i, DeadCodeInterpreter i :<: fs)
-       => ((Term i -> Eff fs (Val i)) -> Term i -> Eff fs (Val i))
-       -> (Term i -> Eff fs (Val i))
-       -> Term i
-       -> Eff fs (Val i)
-evDead ev0 ev e = do
-  modify (Set.delete e)
-  ev0 ev e
-
-
-instance (MonadFail m, AbstractValue i m) => AbstractValue (Val i) m where
-  delta1 o (I a) = fmap I (delta1 o a)
-  delta1 _ _ = fail "non-numeric value"
-
-  delta2 o (I a) (I b) = fmap I (delta2 o a b)
-  delta2 _ _ _ = fail "non-numeric value"
-
-  isZero (I a) = isZero a
-  isZero _ = fail "non-numeric value"
-
-type Interpreter i = '[Failure, State (Store (Val i)), Reader (Environment i)]
-type Trace i f = f (TraceEntry i)
-type TraceEntry i = (Term i, Environment i, Store (Val i))
-type TracingInterpreter i f = Writer (Trace i f) ': Interpreter i
-type TraceInterpreter i = Writer (Trace i []) ': Interpreter i
-type ReachableStateInterpreter i = Writer (Trace i Set.Set) ': Interpreter i
-type DeadCodeInterpreter i = State (Set.Set (Term i)) ': Interpreter i
 
 
 -- Instances
@@ -253,26 +124,3 @@ instance Num i => Num (Term i) where
   negate = Fix . Op1 Negate
   (+) = (Fix .) . Op2 Plus
   (*) = (Fix .) . Op2 Times
-
-instance (Real i, AbstractValue i (Eff (Interpreter i))) => Real (Term i) where
-  toRational term = case fst (eval term) of
-    Right (I a) -> toRational a
-    Right _ -> error "toRational applied to non-numeric Term"
-    Left s -> error s
-
-instance (Enum i, Num i, AbstractValue i (Eff (Interpreter i))) => Enum (Term i) where
-  toEnum = fromIntegral
-  fromEnum term = case fst (eval term) of
-    Right (I a) -> fromEnum a
-    Right _ -> error "fromEnum applied to non-numeric Term"
-    Left s -> error s
-
-instance (Integral i, AbstractValue i (Eff (Interpreter i))) => Integral (Term i) where
-  a `quot` b = Fix (Op2 Quotient a b)
-  a `rem` b = Fix (Op2 Remainder a b)
-  a `quotRem` b = (a `quot` b, a `rem` b)
-
-  toInteger term = case fst (eval term) of
-    Right (I a) -> toInteger a
-    Right _ -> error "toInteger applied to non-numeric Term"
-    Left s -> error s

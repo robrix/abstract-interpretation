38d927e54ce91589fff304e21a060c2bbf6b0e32
Rob Rix <rob.rix@me.com>
2017-08-13 17:02:55 -0400

    Define a Pretty1 class.

--- src/Data/Functor/Classes/Pretty.hs
+++ src/Data/Functor/Classes/Pretty.hs
@@ -1,1 +1,7 @@
 module Data.Functor.Classes.Pretty where
+
+import Data.Text.Prettyprint.Doc
+
+class Pretty1 f where
+  liftPretty :: (a -> Doc ann) -> ([a] -> Doc ann) -> f a -> Doc ann
+  liftPrettyList :: (a -> Doc ann) -> ([a] -> Doc ann) -> [f a] -> Doc ann

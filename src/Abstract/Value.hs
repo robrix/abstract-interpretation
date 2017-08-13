1368bb9d3080858e4b05a69b4e5fcce950fafb8c
Rob Rix <rob.rix@me.com>
2017-08-13 15:45:09 -0400

    Environments map variables to addresses.

--- src/Abstract/Value.hs
+++ src/Abstract/Value.hs
@@ -2,45 +2,45 @@
 module Abstract.Value where
 
 import Abstract.Number
+import Abstract.Store
 import Abstract.Syntax
 import Control.Monad.Fail
 import Data.Functor.Classes
 import qualified Data.Map as Map
 import Data.Semigroup
 import Prelude hiding (fail)
-import Text.Show
 
 type Environment = Map.Map String
 
 data Value l a = I a | Closure String (Term a) (Environment (l (Value l a)))
 
 
-instance Eq1 l => Eq1 (Value l) where
+instance Address l => Eq1 (Value l) where
   liftEq eq = go
     where go (I a) (I b) = a `eq` b
-          go (Closure s1 t1 e1) (Closure s2 t2 e2) = s1 == s2 && liftEqTerms eq t1 t2 && liftEq (liftEq go) e1 e2
+          go (Closure s1 t1 e1) (Closure s2 t2 e2) = s1 == s2 && liftEqTerms eq t1 t2 && liftEq addressEq e1 e2
           go _ _ = False
 
-instance (Eq a, Eq1 l) => Eq (Value l a) where
+instance (Eq a, Address l) => Eq (Value l a) where
   (==) = eq1
 
-instance Ord1 l => Ord1 (Value l) where
+instance Address l => Ord1 (Value l) where
   liftCompare compareA = go
     where go (I a) (I b) = compareA a b
-          go (Closure s1 t1 e1) (Closure s2 t2 e2) = compare s1 s2 <> liftCompareTerms compareA t1 t2 <> liftCompare (liftCompare go) e1 e2
+          go (Closure s1 t1 e1) (Closure s2 t2 e2) = compare s1 s2 <> liftCompareTerms compareA t1 t2 <> liftCompare addressCompare e1 e2
           go (I _) _ = LT
           go _ _ = GT
 
-instance (Ord a, Ord1 l) => Ord (Value l a) where
+instance (Ord a, Address l) => Ord (Value l a) where
   compare = compare1
 
 
-instance Show1 l => Show1 (Value l) where
+instance Address l => Show1 (Value l) where
   liftShowsPrec spA spL = go
     where go d (I a) = showsUnaryWith spA "I" d a
-          go d (Closure s t e) = showsTernaryWith showsPrec (liftShowsPrecTerm spA spL) (liftShowsPrec (liftShowsPrec go (showListWith (go 0))) (liftShowList go (showListWith (go 0)))) "Closure" d s t e
+          go d (Closure s t e) = showsTernaryWith showsPrec (liftShowsPrecTerm spA spL) (liftShowsPrec addressShowsPrec addressShowList) "Closure" d s t e
 
-instance (Show a, Show1 l) => Show (Value l a) where
+instance (Show a, Address l) => Show (Value l a) where
   showsPrec = showsPrec1
 
 

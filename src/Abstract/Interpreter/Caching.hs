937484da136ec533bafbec1f622714c920955035
Rob Rix <rob.rix@me.com>
2017-08-13 13:10:10 -0400

    Split out a CacheIn effect.

--- src/Abstract/Interpreter/Caching.hs
+++ src/Abstract/Interpreter/Caching.hs
@@ -4,6 +4,7 @@ module Abstract.Interpreter.Caching where
 import Abstract.Configuration
 import Abstract.Interpreter
 import Abstract.Store
+import Abstract.Syntax
 import Abstract.Value
 import Control.Effect
 import Control.Monad.Effect.Internal
@@ -11,10 +12,11 @@ import qualified Data.Map as Map
 
 type Cache l a = Map.Map (Configuration l a) (Value l a, Store l (Value l a))
 
-type CachingInterpreter l a = CacheOut l a ': Interpreter l a
+type CachingInterpreter l a = CacheOut l a ': CacheIn l a ': Interpreter l a
 
-askCacheIn :: (CacheOut l a :< fs) => Eff fs (Cache l a)
+askCacheIn :: (CacheIn l a :< fs) => Eff fs (Cache l a)
 askCacheIn = send Ask
+
 
 getCacheOut :: (CacheOut l a :< fs) => Eff fs (Cache l a)
 getCacheOut = send Get
@@ -26,14 +28,18 @@ modifyCacheOut :: (CacheOut l a :< fs) => (Cache l a -> Cache l a) -> Eff fs ()
 modifyCacheOut f = fmap f getCacheOut >>= putCacheOut
 
 
+data CacheIn l a v where
+  Ask :: CacheIn l a (Cache l a)
+
 data CacheOut l a v where
-  Ask :: CacheOut l a (Cache l a)
   Get :: CacheOut l a (Cache l a)
   Put :: !(Cache l a) -> CacheOut l a ()
+
+instance RunEffect (CacheIn l a) where
+  runEffect = relay pure (\ Ask k -> k Map.empty)
 
 instance RunEffect (CacheOut l a) where
   type Result (CacheOut l a) v = (v, Cache l a)
   runEffect = relayState Map.empty ((pure .) . flip (,)) $ \ state eff yield -> case eff of
-    Ask -> yield state state
     Get -> yield state state
     Put state' -> yield state' ()

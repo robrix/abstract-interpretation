70eb63521f595a635abc77e7aef300c6f2fc200d
Rob Rix <rob.rix@me.com>
2017-08-13 17:03:37 -0400

    Define a Pretty2 class.

--- src/Data/Functor/Classes/Pretty.hs
+++ src/Data/Functor/Classes/Pretty.hs
@@ -5,3 +5,7 @@ import Data.Text.Prettyprint.Doc
 class Pretty1 f where
   liftPretty :: (a -> Doc ann) -> ([a] -> Doc ann) -> f a -> Doc ann
   liftPrettyList :: (a -> Doc ann) -> ([a] -> Doc ann) -> [f a] -> Doc ann
+
+class Pretty2 f where
+  liftPretty2 :: (a -> Doc ann) -> ([a] -> Doc ann) -> (b -> Doc ann) -> ([b] -> Doc ann) -> f a b -> Doc ann
+  liftPrettyList2 :: (a -> Doc ann) -> ([a] -> Doc ann) -> (b -> Doc ann) -> ([b] -> Doc ann) -> [f a b] -> Doc ann

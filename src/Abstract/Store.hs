e840011025ce1a31ad02154b4a00d115651a29d4
Rob Rix <rob.rix@me.com>
2017-08-12 23:12:04 -0400

    Represent all stores with the same location type.

--- src/Abstract/Store.hs
+++ src/Abstract/Store.hs
@@ -5,39 +5,33 @@ import Control.Applicative
 import Control.Monad.Effect
 import Control.Monad.Effect.State
 import Data.Foldable (asum)
-import qualified Data.IntMap as IntMap
 import qualified Data.Map as Map
 import Data.Semigroup
 
-type Store v = IntMap.IntMap v
+type Store a = Map.Map (Loc a) a
 
-newtype Loc i = Loc { unLoc :: Int }
+newtype Loc a = Loc { unLoc :: String }
   deriving (Eq, Ord, Show)
 
 find :: (State (Store a) :< fs) => Loc a -> Eff fs a
-find = flip fmap get . flip (IntMap.!) . unLoc
+find = flip fmap get . flip (Map.!)
 
 alloc :: forall a fs . (State (Store a) :< fs) => String -> Eff fs (Loc a)
-alloc _ = do
-  s <- get
-  return (Loc (length (s :: Store a)))
+alloc s = return (Loc s)
 
 ext :: (State (Store a) :< fs) => Loc a -> a -> Eff fs ()
-ext (Loc loc) val = modify (IntMap.insert loc val)
+ext loc val = modify (Map.insert loc val)
 
 
-newtype Loc' a = Loc' { unLoc' :: String }
-  deriving (Eq, Ord, Show)
+type Store' a = Map.Map (Loc a) [a]
 
-type Store' a = Map.Map (Loc' a) [a]
-
-find' :: forall a fs. (Alternative (Eff fs), State (Store' a) :< fs) => Loc' a -> Eff fs a
+find' :: forall a fs. (Alternative (Eff fs), State (Store' a) :< fs) => Loc a -> Eff fs a
 find' loc = do
   store <- get
   asum (return <$> ((store :: Store' a) Map.! loc))
 
-alloc' :: Alternative m => String -> m (Loc' a)
-alloc' x = pure (Loc' x)
+alloc' :: Alternative m => String -> m (Loc a)
+alloc' x = pure (Loc x)
 
-ext' :: (State (Store' a) :< fs) => Loc' a -> a -> Eff fs ()
+ext' :: (State (Store' a) :< fs) => Loc a -> a -> Eff fs ()
 ext' loc val = modify (Map.insertWith (<>) loc [val])

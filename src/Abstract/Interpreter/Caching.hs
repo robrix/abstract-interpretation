763500ccca4af8ab9d2eae683682e9e1343b13ce
Rob Rix <rob.rix@me.com>
2017-08-13 13:02:08 -0400

    Rename Caching to CacheOut.

--- src/Abstract/Interpreter/Caching.hs
+++ src/Abstract/Interpreter/Caching.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, GADTs, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
 module Abstract.Interpreter.Caching where
 
 import Abstract.Configuration
@@ -11,26 +11,26 @@ import qualified Data.Map as Map
 type Cache l a = Map.Map (Configuration l a) (Value l a, Store l (Value l a))
 
 
-askCacheIn :: (Caching l a :< fs) => Eff fs (Cache l a)
+askCacheIn :: (CacheOut l a :< fs) => Eff fs (Cache l a)
 askCacheIn = send Ask
 
-getCacheOut :: (Caching l a :< fs) => Eff fs (Cache l a)
+getCacheOut :: (CacheOut l a :< fs) => Eff fs (Cache l a)
 getCacheOut = send Get
 
-putCacheOut :: (Caching l a :< fs) => Cache l a -> Eff fs ()
+putCacheOut :: (CacheOut l a :< fs) => Cache l a -> Eff fs ()
 putCacheOut s = send (Put s)
 
-modifyCacheOut :: (Caching l a :< fs) => (Cache l a -> Cache l a) -> Eff fs ()
+modifyCacheOut :: (CacheOut l a :< fs) => (Cache l a -> Cache l a) -> Eff fs ()
 modifyCacheOut f = fmap f getCacheOut >>= putCacheOut
 
 
-data Caching l a v where
-  Ask :: Caching l a (Cache l a)
-  Get :: Caching l a (Cache l a)
-  Put :: !(Cache l a) -> Caching l a ()
+data CacheOut l a v where
+  Ask :: CacheOut l a (Cache l a)
+  Get :: CacheOut l a (Cache l a)
+  Put :: !(Cache l a) -> CacheOut l a ()
 
-instance RunEffect (Caching l a) where
-  type Result (Caching l a) v = (v, Cache l a)
+instance RunEffect (CacheOut l a) where
+  type Result (CacheOut l a) v = (v, Cache l a)
   runEffect = relayState Map.empty ((pure .) . flip (,)) $ \ state eff yield -> case eff of
     Ask -> yield state state
     Get -> yield state state

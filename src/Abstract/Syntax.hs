d1cb5496f948a7d02627a73470249477545d0920
Rob Rix <rob.rix@me.com>
2017-08-13 18:04:18 -0400

    Print variables as just their names.

--- src/Abstract/Syntax.hs
+++ src/Abstract/Syntax.hs
@@ -173,7 +173,7 @@ instance Num i => Num (Term i) where
 
 instance Pretty n => Pretty2 (Syntax n) where
   liftPretty2 pv _ pr _ s = case s of
-    Var n -> pretty "var" <+> pretty n
+    Var n -> pretty n
     Num v -> pv v
     Op1 o a -> pretty o <+> pr a
     Op2 o a b -> pr a <+> pretty o <+> pr b

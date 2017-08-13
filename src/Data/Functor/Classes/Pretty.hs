e1e1ebba014be709f3452858f248feaaeab06c8a
Rob Rix <rob.rix@me.com>
2017-08-13 17:07:28 -0400

    Give default definitions for liftPrettyList and liftPrettyList2.

--- src/Data/Functor/Classes/Pretty.hs
+++ src/Data/Functor/Classes/Pretty.hs
@@ -5,10 +5,12 @@ import Data.Text.Prettyprint.Doc
 class Pretty1 f where
   liftPretty :: (a -> Doc ann) -> ([a] -> Doc ann) -> f a -> Doc ann
   liftPrettyList :: (a -> Doc ann) -> ([a] -> Doc ann) -> [f a] -> Doc ann
+  liftPrettyList p pl = encloseSep lbracket rbracket (comma <> space) . map (liftPretty p pl)
 
 class Pretty2 f where
   liftPretty2 :: (a -> Doc ann) -> ([a] -> Doc ann) -> (b -> Doc ann) -> ([b] -> Doc ann) -> f a b -> Doc ann
   liftPrettyList2 :: (a -> Doc ann) -> ([a] -> Doc ann) -> (b -> Doc ann) -> ([b] -> Doc ann) -> [f a b] -> Doc ann
+  liftPrettyList2 pa pla pb plb = encloseSep lbracket rbracket (comma <> space) . map (liftPretty2 pa pla pb plb)
 
 pretty1 :: (Pretty a, Pretty1 f) => f a -> Doc ann
 pretty1 = liftPretty pretty prettyList

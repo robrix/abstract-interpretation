46cf4bf3559366d78b32c463e888de746da17ce6
Rob Rix <rob.rix@me.com>
2017-08-13 12:23:39 -0400

    Define a Cache type synonym.

--- src/Abstract/Interpreter/Caching.hs
+++ src/Abstract/Interpreter/Caching.hs
@@ -1,1 +1,8 @@
 module Abstract.Interpreter.Caching where
+
+import Abstract.Configuration
+import Abstract.Store
+import Abstract.Value
+import qualified Data.Map as Map
+
+type Cache l a = Map.Map (Configuration l a) (Value l a, Store l (Value l a))

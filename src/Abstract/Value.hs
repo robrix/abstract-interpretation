b1d319f7efa2f9005cc92f4ac9e7ff2f31419156
Rob Rix <rob.rix@me.com>
2017-08-13 15:08:38 -0400

    Define lifted equality of Values.

--- src/Abstract/Value.hs
+++ src/Abstract/Value.hs
@@ -4,6 +4,7 @@ module Abstract.Value where
 import Abstract.Number
 import Abstract.Syntax
 import Control.Monad.Fail
+import Data.Functor.Classes
 import qualified Data.Map as Map
 import Prelude hiding (fail)
 
@@ -11,10 +12,18 @@ type Environment = Map.Map String
 
 data Value l a = I a | Closure String (Term a) (Environment (l (Value l a)))
 
-deriving instance (Eq a, Eq (l (Value l a))) => Eq (Value l a)
-deriving instance (Ord a, Ord (l (Value l a))) => Ord (Value l a)
+deriving instance (Eq1 l, Ord a, Ord (l (Value l a))) => Ord (Value l a)
 deriving instance (Show a, Show (l (Value l a))) => Show (Value l a)
 
+
+instance Eq1 l => Eq1 (Value l) where
+  liftEq eq = go
+    where go (I a) (I b) = a `eq` b
+          go (Closure s1 t1 e1) (Closure s2 t2 e2) = s1 == s2 && liftEqTerms eq t1 t2 && liftEq (liftEq go) e1 e2
+          go _ _ = False
+
+instance (Eq a, Eq1 l) => Eq (Value l a) where
+  (==) = eq1
 
 instance (MonadFail m, AbstractNumber i m) => AbstractNumber (Value l i) m where
   delta1 o (I a) = fmap I (delta1 o a)

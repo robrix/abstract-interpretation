333a4b94840acedca06aa5e75211096dfdf570ec
Rob Rix <rob.rix@me.com>
2017-08-20 17:53:33 -0400

    Define a unary eliminator for Sym.

--- src/Abstract/Interpreter/Symbolic.hs
+++ src/Abstract/Interpreter/Symbolic.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
 module Abstract.Interpreter.Symbolic where
 
 import Abstract.Interpreter
 data Sym a = Sym String | V a
   deriving (Eq, Ord, Show)
+
+sym :: Num a => (forall n . Num n => n -> n) -> Sym a -> Sym a
+sym f (Sym t) = Sym (f t)
+sym f (V a) = V (f a)
 
 evSymbolic :: (Eval l fs (Sym a) -> Eval l fs (Sym a)) -> Eval l fs (Sym a) -> Eval l fs (Sym a)
 evSymbolic ev0 ev e = ev0 ev e

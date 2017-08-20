54c6cd150d91a98bb02b7ea1e4861ff9a2e0409f
Rob Rix <rob.rix@me.com>
2017-08-20 17:08:47 -0400

    Stub in a symbolic evaluator.

--- src/Abstract/Interpreter/Symbolic.hs
+++ src/Abstract/Interpreter/Symbolic.hs
@@ -1,4 +1,8 @@
 module Abstract.Interpreter.Symbolic where
 
+import Abstract.Interpreter
 data Sym a = Sym String | V a
   deriving (Eq, Ord, Show)
+
+evSymbolic :: (Eval l fs (Sym a) -> Eval l fs (Sym a)) -> Eval l fs (Sym a) -> Eval l fs (Sym a)
+evSymbolic ev0 ev e = ev0 ev e

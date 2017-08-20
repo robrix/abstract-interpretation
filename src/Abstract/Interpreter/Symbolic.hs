b3bbb23f35a86b330a797f1f2185c0c6528103d6
Rob Rix <rob.rix@me.com>
2017-08-20 17:53:40 -0400

    Define a binary eliminator for Sym.

--- src/Abstract/Interpreter/Symbolic.hs
+++ src/Abstract/Interpreter/Symbolic.hs
@@ -9,5 +9,11 @@ sym :: Num a => (forall n . Num n => n -> n) -> Sym a -> Sym a
 sym f (Sym t) = Sym (f t)
 sym f (V a) = V (f a)
 
+sym2 :: Num a => (forall n . Num n => n -> n -> n) -> Sym a -> Sym a -> Sym a
+sym2 f (V a) (V b) = V (f a b)
+sym2 f (Sym a) (Sym b) = Sym (f a b)
+sym2 f (Sym a) (V b) = Sym (f a (num b))
+sym2 f (V a) (Sym b) = Sym (f (num a) b)
+
 evSymbolic :: (Eval l fs (Sym a) -> Eval l fs (Sym a)) -> Eval l fs (Sym a) -> Eval l fs (Sym a)
 evSymbolic ev0 ev e = ev0 ev e

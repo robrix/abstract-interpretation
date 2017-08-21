12a089559ba3ea8f1a78419c5b7e3c68ad3ed5b8
Rob Rix <rob.rix@me.com>
2017-08-21 13:09:58 -0400

    Generalize runDead over the evaluator & term type.

--- src/Abstract/Interpreter/Dead.hs
+++ src/Abstract/Interpreter/Dead.hs
@@ -4,12 +4,14 @@ module Abstract.Interpreter.Dead where
 import Abstract.Interpreter
 import Abstract.Number
 import Abstract.Store
-import Abstract.Syntax
+import Abstract.Syntax hiding (subterms)
 import Abstract.Value
 import Control.Effect
 import Control.Monad.Effect hiding (run)
 import Control.Monad.Effect.State
 import Data.Function (fix)
+import Data.Functor.Foldable
+import Data.Semigroup
 import qualified Data.Set as Set
 
 type DeadCodeInterpreter l t v = State (Dead t) ': Interpreter l v
@@ -23,13 +25,17 @@ revive = (Dead .) . (. unDead) . Set.delete
 type DeadCodeResult l a = (Either String (Value l (Term a) a, Dead (Term a)), Store l (Value l (Term a) a))
 
 
+subterms :: (Ord a, Recursive a, Foldable (Base a)) => a -> Set.Set a
+subterms term = para (foldMap (uncurry ((<>) . Set.singleton))) term <> Set.singleton term
+
+
 -- Dead code analysis
 
 evalDead :: forall l a. (Ord a, Address l, Context l (Value l (Term a) a) (DeadCodeInterpreter l (Term a) (Value l (Term a) a)), AbstractNumber a (Eff (DeadCodeInterpreter l (Term a) (Value l (Term a) a)))) => Term a -> DeadCodeResult l a
-evalDead = run @(DeadCodeInterpreter l (Term a) (Value l (Term a) a)) . runDead
+evalDead = run @(DeadCodeInterpreter l (Term a) (Value l (Term a) a)) . runDead ev
 
-runDead :: (Ord a, DeadCodeInterpreter l (Term a) (Value l (Term a) a) :<: fs, Address l, Context l (Value l (Term a) a) fs, AbstractNumber a (Eff fs)) => Eval (Term a) fs (Value l (Term a) a)
-runDead e0 = do
+runDead :: (Ord a, Ord t, Recursive t, Foldable (Base t), DeadCodeInterpreter l t (Value l t a) :<: fs, Address l, Context l (Value l t a) fs, AbstractNumber a (Eff fs)) => (Eval t fs (Value l t a) -> Eval t fs (Value l t a)) -> Eval t fs (Value l t a)
+runDead ev e0 = do
   put (Dead (subterms e0))
   fix (evDead ev) e0
 

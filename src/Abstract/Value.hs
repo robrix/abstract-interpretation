f008f86cadd0997126cf87d0dd17bf387361cce4
Rob Rix <rob.rix@me.com>
2017-08-21 09:34:49 -0400

    Tighten up the Eq1, Ord1, & Show1 instances for Value.

--- src/Abstract/Value.hs
+++ src/Abstract/Value.hs
@@ -25,29 +25,29 @@ data Value l a = I a | Closure Name (Term a) (Environment (l (Value l a)))
 
 
 instance Address l => Eq1 (Value l) where
-  liftEq eq = go
-    where go (I a) (I b) = a `eq` b
-          go (Closure s1 t1 e1) (Closure s2 t2 e2) = s1 == s2 && liftEqTerms eq t1 t2 && liftEq addressEq e1 e2
-          go _ _ = False
+  liftEq eq v1 v2 = case (v1, v2) of
+    (I a, I b) -> a `eq` b
+    (Closure s1 t1 e1, Closure s2 t2 e2) -> s1 == s2 && liftEqTerms eq t1 t2 && liftEq addressEq e1 e2
+    _ -> False
 
 instance (Eq a, Address l) => Eq (Value l a) where
   (==) = eq1
 
 instance Address l => Ord1 (Value l) where
-  liftCompare compareA = go
-    where go (I a) (I b) = compareA a b
-          go (Closure s1 t1 e1) (Closure s2 t2 e2) = compare s1 s2 <> liftCompareTerms compareA t1 t2 <> liftCompare addressCompare e1 e2
-          go (I _) _ = LT
-          go _ _ = GT
+  liftCompare compareA v1 v2 = case (v1, v2) of
+    (I a, I b) -> compareA a b
+    (Closure s1 t1 e1, Closure s2 t2 e2) -> compare s1 s2 <> liftCompareTerms compareA t1 t2 <> liftCompare addressCompare e1 e2
+    (I _, _) -> LT
+    _ -> GT
 
 instance (Ord a, Address l) => Ord (Value l a) where
   compare = compare1
 
 
 instance Address l => Show1 (Value l) where
-  liftShowsPrec spA spL = go
-    where go d (I a) = showsUnaryWith spA "I" d a
-          go d (Closure s t e) = showsTernaryWith showsPrec (liftShowsPrecTerm spA spL) (liftShowsPrec addressShowsPrec addressShowList) "Closure" d s t e
+  liftShowsPrec spA spL d v = case v of
+    I a -> showsUnaryWith spA "I" d a
+    Closure s t e -> showsTernaryWith showsPrec (liftShowsPrecTerm spA spL) (liftShowsPrec addressShowsPrec addressShowList) "Closure" d s t e
 
 instance (Show a, Address l) => Show (Value l a) where
   showsPrec = showsPrec1

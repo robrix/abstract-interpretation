3e4a55e3f446a7df2bc2a6f1d1946679572cd65d
Rob Rix <rob.rix@me.com>
2017-08-21 13:41:12 -0400

    Generalize Sym over the term type.

--- src/Abstract/Interpreter/Symbolic.hs
+++ src/Abstract/Interpreter/Symbolic.hs
@@ -12,20 +12,20 @@ import Control.Monad.Effect.Amb
 import Control.Monad.Effect.State
 import qualified Data.Set as Set
 
-data Sym a = Sym (Term a) | V a
+data Sym t a = Sym t | V a
   deriving (Eq, Ord, Show)
 
-sym :: Num a => (forall n . Num n => n -> n) -> Sym a -> Sym a
+sym :: (Num a, Num t) => (forall n . Num n => n -> n) -> Sym t a -> Sym t a
 sym f (Sym t) = Sym (f t)
 sym f (V a) = V (f a)
 
-sym2 :: Applicative f => (a -> a -> f a) -> (Term a -> Term a -> Term a) -> Sym a -> Sym a -> f (Sym a)
-sym2 f _ (V a) (V b) = V <$> f a b
-sym2 _ g (Sym a) (Sym b) = pure (Sym (g a b))
-sym2 f g a (V b) = sym2 f g a (Sym (num b))
-sym2 f g (V a) b = sym2 f g (Sym (num a)) b
+sym2 :: Applicative f => (a -> a -> f a) -> (a -> t) -> (t -> t -> t) -> Sym t a -> Sym t a -> f (Sym t a)
+sym2 f _ _ (V a) (V b) = V <$> f a b
+sym2 _ _ g (Sym a) (Sym b) = pure (Sym (g a b))
+sym2 f num g a (V b) = sym2 f num g a (Sym (num b))
+sym2 f num g (V a) b = sym2 f num g (Sym (num a)) b
 
-evSymbolic :: (Eval t fs (Value l t (Sym a)) -> Eval t fs (Value l t (Sym a))) -> Eval t fs (Value l t (Sym a)) -> Eval t fs (Value l t (Sym a))
+evSymbolic :: (Eval t fs (Value l t (Sym t a)) -> Eval t fs (Value l t (Sym t a))) -> Eval t fs (Value l t (Sym t a)) -> Eval t fs (Value l t (Sym t a))
 evSymbolic ev0 ev e = ev0 ev e
 
 
@@ -48,19 +48,19 @@ pathConditionInsert :: Ord a => PathExpression a -> PathCondition a -> PathCondi
 pathConditionInsert = ((PathCondition .) . (. unPathCondition)) . Set.insert
 
 
-instance (Num a, Ord a, AbstractNumber a (Eff fs), State (PathCondition a) :< fs, Amb :< fs) => AbstractNumber (Sym a) (Eff fs) where
+instance (Num a, Ord a, AbstractNumber a (Eff fs), State (PathCondition a) :< fs, Amb :< fs) => AbstractNumber (Sym (Term a) a) (Eff fs) where
   delta1 o a = pure $ case o of
     Negate -> sym negate a
     Abs    -> sym abs    a
     Signum -> sym signum a
 
   delta2 o a b = case o of
-    Plus  -> sym2 (delta2 Plus ) (+) a b
-    Minus -> sym2 (delta2 Minus) (-) a b
-    Times -> sym2 (delta2 Times) (*) a b
-    DividedBy -> isZero b >>= flip when divisionByZero >> sym2 (delta2 DividedBy) ((In .) . Op2 DividedBy) a b
-    Quotient  -> isZero b >>= flip when divisionByZero >> sym2 (delta2 Quotient)  ((In .) . Op2 Quotient)  a b
-    Remainder -> isZero b >>= flip when divisionByZero >> sym2 (delta2 Remainder) ((In .) . Op2 Remainder) a b
+    Plus  -> sym2 (delta2 Plus ) num (+) a b
+    Minus -> sym2 (delta2 Minus) num (-) a b
+    Times -> sym2 (delta2 Times) num (*) a b
+    DividedBy -> isZero b >>= flip when divisionByZero >> sym2 (delta2 DividedBy) num ((In .) . Op2 DividedBy) a b
+    Quotient  -> isZero b >>= flip when divisionByZero >> sym2 (delta2 Quotient)  num ((In .) . Op2 Quotient)  a b
+    Remainder -> isZero b >>= flip when divisionByZero >> sym2 (delta2 Remainder) num ((In .) . Op2 Remainder) a b
 
   isZero (V a) = isZero a
   isZero (Sym e) = do

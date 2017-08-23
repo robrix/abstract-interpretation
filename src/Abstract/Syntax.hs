33af857f9d30e57cf8b1b417d898d6b5db0fd3c6
Rob Rix <rob.rix@me.com>
2017-08-22 22:07:51 -0400

    Define a Primitive instance for Term.

--- src/Abstract/Syntax.hs
+++ src/Abstract/Syntax.hs
@@ -255,3 +255,10 @@ instance Pretty2 Syntax where
 
 instance Pretty a => Pretty1 (Syntax a) where
   liftPretty = liftPretty2 pretty prettyList
+
+instance Primitive a m => Primitive (Term a) m where
+  delta1 op = pure . unary op
+  delta2 op = (pure .) . binary op
+
+  truthy (In (Prim a)) = truthy a
+  truthy _ = fail "testing truthiness of unevaluated term"

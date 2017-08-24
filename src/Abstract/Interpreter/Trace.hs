87023f80c40794968fc3ff13a4869f839bd40d47
Rob Rix <rob.rix@me.com>
2017-08-24 12:17:49 -0400

    Use AllowAmbiguousTypes to get rid of the trace interpreter proxies.

--- src/Abstract/Interpreter/Trace.hs
+++ src/Abstract/Interpreter/Trace.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeFamilies, TypeOperators #-}
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, FlexibleContexts, MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeFamilies, TypeOperators #-}
 module Abstract.Interpreter.Trace where
 
 import Abstract.Configuration
@@ -13,7 +13,6 @@ import Control.Monad.Effect.Reader
 import Control.Monad.Effect.State
 import Control.Monad.Effect.Writer
 import Data.Function (fix)
-import Data.Proxy
 import qualified Data.Set as Set
 import GHC.Exts (IsList(..))
 
@@ -27,25 +26,24 @@ type TraceResult l t v f = (Either String (v, f (Configuration l t v)), Store l 
 
 -- Tracing and reachable state analyses
 
-evalTrace :: forall a l. (Address l, Context l (Value l (Term a) a) (Eff (TraceInterpreter l (Term a) (Value l (Term a) a))), PrimitiveOperations a (Eff (TraceInterpreter l (Term a) (Value l (Term a) a)))) => Term a -> TraceResult l (Term a) (Value l (Term a) a) []
-evalTrace = run @(TraceInterpreter l (Term a) (Value l (Term a) a)) . runTrace (Proxy :: Proxy l) ev
+evalTrace :: forall l a. (Address l, Context l (Value l (Term a) a) (Eff (TraceInterpreter l (Term a) (Value l (Term a) a))), PrimitiveOperations a (Eff (TraceInterpreter l (Term a) (Value l (Term a) a)))) => Eval (Term a) (TraceResult l (Term a) (Value l (Term a) a) [])
+evalTrace = run @(TraceInterpreter l (Term a) (Value l (Term a) a)) . runTrace @l ev
 
-runTrace :: (TraceInterpreter l t v :<: fs, Address l) => proxy l -> (Eval t (Eff fs v) -> Eval t (Eff fs v)) -> Eval t (Eff fs v)
-runTrace proxy ev = fix (evTell proxy (Proxy :: Proxy []) ev)
+runTrace :: forall l t v fs. (TraceInterpreter l t v :<: fs, Address l) => (Eval t (Eff fs v) -> Eval t (Eff fs v)) -> Eval t (Eff fs v)
+runTrace ev = fix (evTell @l @t @v @[] ev)
 
-evalReach :: forall a l. (Ord a, Address l, Context l (Value l (Term a) a) (Eff (ReachableStateInterpreter l (Term a) (Value l (Term a) a))), PrimitiveOperations a (Eff (ReachableStateInterpreter l (Term a) (Value l (Term a) a)))) => Term a -> TraceResult l (Term a) (Value l (Term a) a) Set.Set
-evalReach = run @(ReachableStateInterpreter l (Term a) (Value l (Term a) a)) . runReach (Proxy :: Proxy l) ev
+evalReach :: forall l a. (Ord a, Address l, Context l (Value l (Term a) a) (Eff (ReachableStateInterpreter l (Term a) (Value l (Term a) a))), PrimitiveOperations a (Eff (ReachableStateInterpreter l (Term a) (Value l (Term a) a)))) => Eval (Term a) (TraceResult l (Term a) (Value l (Term a) a) Set.Set)
+evalReach = run @(ReachableStateInterpreter l (Term a) (Value l (Term a) a)) . runReach @l ev
 
-runReach :: (Ord t, Ord v, ReachableStateInterpreter l t v :<: fs, Address l) => proxy l -> (Eval t (Eff fs v) -> Eval t (Eff fs v)) -> Eval t (Eff fs v)
-runReach proxy ev = fix (evTell proxy (Proxy :: Proxy Set.Set) ev)
+runReach :: forall l t v fs. (ReachableStateInterpreter l t v :<: fs, Ord t, Ord v, Address l) => (Eval t (Eff fs v) -> Eval t (Eff fs v)) -> Eval t (Eff fs v)
+runReach ev = fix (evTell @l @t @v @Set.Set ev)
 
-evTell :: forall l t v g fs proxy proxy' . (TracingInterpreter l t v g :<: fs, IsList (g (Configuration l t v)), Item (g (Configuration l t v)) ~ Configuration l t v)
-       => proxy l
-       -> proxy' g
-       -> (Eval t (Eff fs v) -> Eval t (Eff fs v))
+evTell :: forall l t v g fs
+       .  (TracingInterpreter l t v g :<: fs, IsList (g (Configuration l t v)), Item (g (Configuration l t v)) ~ Configuration l t v)
+       => (Eval t (Eff fs v) -> Eval t (Eff fs v))
        -> Eval t (Eff fs v)
        -> Eval t (Eff fs v)
-evTell _ _ ev0 ev e = do
+evTell ev0 ev e = do
   env <- ask
   store <- get
   tell (fromList [Configuration e env store] :: g (Configuration l t v))

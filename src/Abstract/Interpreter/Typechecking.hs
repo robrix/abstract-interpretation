2af460f4ba4bbcaf9048321d58b6be5bd6fae997
Rob Rix <rob.rix@me.com>
2017-08-25 10:25:49 -0400

    Specialize evalCheck to Prim/Type.

--- src/Abstract/Interpreter/Typechecking.hs
+++ src/Abstract/Interpreter/Typechecking.hs
@@ -5,25 +5,26 @@ import Abstract.Interpreter
 import Abstract.Primitive
 import Abstract.Store
 import Abstract.Syntax
-import Abstract.Value
+import Abstract.Type
 import Control.Effect
 import Control.Monad.Effect hiding (run)
+import Control.Monad.Effect.Amb
 import Data.Function (fix)
 
-type TypecheckingInterpreter l v = Interpreter l v
+type TypecheckingInterpreter l = Amb ': Interpreter l Type
 
-type TypecheckingResult l v = (Either String v, Store l v)
+type TypecheckingResult l = (Either String [Type], Store l Type)
 
-evalCheck :: forall l v a
-          .  (Ord a, Address l, Context l v (Eff (TypecheckingInterpreter l v)), AbstractValue l v Term a, PrimitiveOperations v (Eff (TypecheckingInterpreter l v)))
-          => Eval (Term a) (TypecheckingResult l v)
-evalCheck = run @(TypecheckingInterpreter l v) . runCheck @l (ev @l)
+evalCheck :: forall l
+          .  (Address l, Context l Type (Eff (TypecheckingInterpreter l)))
+          => Eval (Term Prim) (TypecheckingResult l)
+evalCheck = run @(TypecheckingInterpreter l) . runCheck @l (ev @l)
 
-runCheck :: forall l t v fs. TypecheckingInterpreter l v :<: fs => (Eval t (Eff fs v) -> Eval t (Eff fs v)) -> Eval t (Eff fs v)
+runCheck :: forall l t fs. TypecheckingInterpreter l :<: fs => (Eval t (Eff fs Type) -> Eval t (Eff fs Type)) -> Eval t (Eff fs Type)
 runCheck ev e0 = fix (evCheck @l ev) e0
 
-evCheck :: TypecheckingInterpreter l v :<: fs
-        => (Eval t (Eff fs v) -> Eval t (Eff fs v))
-        -> Eval t (Eff fs v)
-        -> Eval t (Eff fs v)
+evCheck :: TypecheckingInterpreter l :<: fs
+        => (Eval t (Eff fs Type) -> Eval t (Eff fs Type))
+        -> Eval t (Eff fs Type)
+        -> Eval t (Eff fs Type)
 evCheck ev0 ev e = ev0 ev e

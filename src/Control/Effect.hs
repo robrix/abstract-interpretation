55eabbdd94468bcb45829cd34f1888a7a2fe0a0f
Rob Rix <rob.rix@me.com>
2017-08-26 18:30:33 -0400

    Rename Result to Out.

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -16,35 +16,35 @@ class RunEffects fs a where
   type Final fs a
   runEffects :: Eff fs a -> Eff '[] (Final fs a)
 
-instance (RunEffect f1 a, RunEffects (f2 ': fs) (Result f1 a)) => RunEffects (f1 ': f2 ': fs) a where
-  type Final (f1 ': f2 ': fs) a = Final (f2 ': fs) (Result f1 a)
+instance (RunEffect f1 a, RunEffects (f2 ': fs) (Out f1 a)) => RunEffects (f1 ': f2 ': fs) a where
+  type Final (f1 ': f2 ': fs) a = Final (f2 ': fs) (Out f1 a)
   runEffects = runEffects . runEffect
 
 instance RunEffect f a => RunEffects '[f] a where
-  type Final '[f] a = Result f a
+  type Final '[f] a = Out f a
   runEffects = runEffect
 
 
 class RunEffect f a where
-  type Result f a
-  type instance Result f a = a
-  runEffect :: Eff (f ': fs) a -> Eff fs (Result f a)
+  type Out f a
+  type instance Out f a = a
+  runEffect :: Eff (f ': fs) a -> Eff fs (Out f a)
 
 instance Monoid b => RunEffect (State b) a where
-  type Result (State b) a = (a, b)
+  type Out (State b) a = (a, b)
   runEffect = flip runState mempty
 
 instance Monoid b => RunEffect (Reader b) a where
   runEffect = flip runReader mempty
 
 instance RunEffect Failure a where
-  type Result Failure a = Either String a
+  type Out Failure a = Either String a
   runEffect = runFailure
 
 instance Monoid w => RunEffect (Writer w) a where
-  type Result (Writer w) a = (a, w)
+  type Out (Writer w) a = (a, w)
   runEffect = runWriter
 
 instance RunEffect NonDetEff a where
-  type Result NonDetEff a = [a]
+  type Out NonDetEff a = [a]
   runEffect = makeChoiceA

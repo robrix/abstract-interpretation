04ea51fe06b10c82dade6c5ff585639d11e1ce15
Rob Rix <rob.rix@me.com>
2017-08-28 11:38:22 -0400

    Stub in a collecting evaluator.

--- src/Abstract/Interpreter/Collecting.hs
+++ src/Abstract/Interpreter/Collecting.hs
@@ -1,12 +1,24 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables, TypeOperators, UndecidableInstances #-}
 module Abstract.Interpreter.Collecting where
 
+import Abstract.Interpreter
 import Abstract.RootSet
 import Control.Monad.Effect
 import Control.Monad.Effect.Reader
 
-class MonadGC l a m where
+class Monad m => MonadGC l a m where
   askRoots :: m (RootSet l a)
 
 instance Reader (RootSet l a) :< fs => MonadGC l a (Eff fs) where
   askRoots = ask
+
+
+evCollect :: forall l t v m
+          .  MonadGC l v m
+          => (Eval t (m v) -> Eval t (m v))
+          -> Eval t (m v)
+          -> Eval t (m v)
+evCollect ev0 ev e = do
+  _ <- askRoots :: m (RootSet l v)
+  v <- ev0 ev e
+  return v

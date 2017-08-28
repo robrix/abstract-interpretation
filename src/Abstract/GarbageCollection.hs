a105655b804312538136fa1be28f5655749e4aae
Rob Rix <rob.rix@me.com>
2017-08-28 10:41:22 -0400

    Stub in a GC monad typeclass.

--- src/Abstract/GarbageCollection.hs
+++ src/Abstract/GarbageCollection.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Abstract.GarbageCollection where
+
+import Abstract.Set
+import Abstract.Store
+
+class MonadGC l a m where
+  askRoots :: m (Set (Address l a))

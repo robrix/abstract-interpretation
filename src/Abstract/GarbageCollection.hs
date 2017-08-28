6eff643ab185f79344c14e20c78ec9dcbeb9c5f0
Rob Rix <rob.rix@me.com>
2017-08-28 10:51:20 -0400

    Define a Roots newtype.

--- src/Abstract/GarbageCollection.hs
+++ src/Abstract/GarbageCollection.hs
@@ -1,8 +1,12 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
 module Abstract.GarbageCollection where
 
 import Abstract.Set
 import Abstract.Store
+import Data.Semigroup
+
+newtype Roots l a = Roots { unRoots :: Set (Address l a) }
+  deriving (Eq, Foldable, Monoid, Ord, Semigroup, Show)
 
 class MonadGC l a m where
   askRoots :: m (Set (Address l a))

330c3e9fc2610a768ba4ff2b1a2b3928ee0b9b98
Rob Rix <rob.rix@me.com>
2017-08-28 13:20:08 -0400

    Stub in collecting of addresses.

--- src/Abstract/Interpreter/Collecting.hs
+++ src/Abstract/Interpreter/Collecting.hs
@@ -3,8 +3,11 @@ module Abstract.Interpreter.Collecting where
 
 import Abstract.Interpreter
 import Abstract.RootSet
+import Abstract.Store
+import Abstract.Value
 import Control.Monad.Effect
 import Control.Monad.Effect.Reader
+import Data.Semigroup
 
 class Monad m => MonadGC l a m where
   askRoots :: m (RootSet l a)
@@ -13,12 +16,17 @@ instance Reader (RootSet l a) :< fs => MonadGC l a (Eff fs) where
   askRoots = ask
 
 
+gc :: RootSet l a -> Store l a -> Store l a
+gc = const id
+
+
 evCollect :: forall l t v m
-          .  MonadGC l v m
+          .  (Ord l, MonadStore l v m, MonadGC l v m, MonadValue l v t m)
           => (Eval t (m v) -> Eval t (m v))
           -> Eval t (m v)
           -> Eval t (m v)
 evCollect ev0 ev e = do
-  _ <- askRoots :: m (RootSet l v)
+  roots <- askRoots :: m (RootSet l v)
   v <- ev0 ev e
+  modifyStore (gc (roots <> valueRoots v))
   return v

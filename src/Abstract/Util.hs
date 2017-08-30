5bd000b732bb8839e2470620da33f2a71ab6a3f9
Rob Rix <rob.rix@me.com>
2017-08-30 12:16:34 -0400

    Add a facility for pretty-printing to a String.

--- src/Abstract/Util.hs
+++ src/Abstract/Util.hs
@@ -1,1 +1,7 @@
 module Abstract.Util where
+
+import Data.Text.Prettyprint.Doc
+import Data.Text.Prettyprint.Doc.Render.String
+
+prettyString :: Pretty a => a -> String
+prettyString a = renderString (layoutPretty (LayoutOptions { layoutPageWidth = AvailablePerLine 100 1 }) (unAnnotate (pretty a)))

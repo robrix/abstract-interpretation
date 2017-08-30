37df844d42782ccaf77a1133cc34786cb6d9d662
Rob Rix <rob.rix@me.com>
2017-08-30 09:24:51 -0400

    Add type variables.

--- src/Abstract/Type.hs
+++ src/Abstract/Type.hs
@@ -2,7 +2,9 @@ module Abstract.Type where
 
 import Data.Text.Prettyprint.Doc
 
-data Type = Int | Bool | Type :-> Type | Type :* Type
+type TName = String
+
+data Type = Int | Bool | Type :-> Type | Type :* Type | TVar TName
   deriving (Eq, Ord, Show)
 
 

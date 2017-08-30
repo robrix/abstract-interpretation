c6d6658798d8a55e06f095c5139bad317d39d3bd
Rob Rix <rob.rix@me.com>
2017-08-30 09:37:21 -0400

    Define a MonadFresh instance with the Fresh effect.

--- src/Abstract/Type.hs
+++ src/Abstract/Type.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleContexts, TypeOperators, UndecidableInstances #-}
 module Abstract.Type where
 
+import Control.Monad.Effect
+import Control.Monad.Effect.Fresh as Fresh
 import Data.Text.Prettyprint.Doc
 
 type TName = Int
@@ -10,6 +13,9 @@ data Type = Int | Bool | Type :-> Type | Type :* Type | TVar TName
 
 class Monad m => MonadFresh m where
   fresh :: m TName
+
+instance Fresh :< fs => MonadFresh (Eff fs) where
+  fresh = Fresh.fresh
 
 
 instance Pretty Type where

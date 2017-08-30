c5d487325a772fb5abd88116de9028c75c61d723
Rob Rix <rob.rix@me.com>
2017-08-30 12:21:35 -0400

    Make a half-hearted attempt at unifying variables.

--- src/Abstract/Type.hs
+++ src/Abstract/Type.hs
@@ -19,6 +19,8 @@ unify Int  Int  = pure Int
 unify Bool Bool = pure Bool
 unify (a1 :-> b1) (a2 :-> b2) = (:->) <$> unify a1 a2 <*> unify b1 b2
 unify (a1 :* b1)  (a2 :* b2)  = (:*)  <$> unify a1 a2 <*> unify b1 b2
+unify (TVar _) b = pure b
+unify a (TVar _) = pure a
 unify t1 t2 = fail ("cannot unify " ++ prettyString t1 ++ " with " ++ prettyString t2)
 
 

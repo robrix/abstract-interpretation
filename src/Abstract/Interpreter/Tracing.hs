789f2e9de2c84a01a36c4e83b0e36e981dd47fa2
Timothy Clem <timothy.clem@gmail.com>
2017-09-29 13:54:40 -0700

    Hello tracing interpreter

--- src/Abstract/Interpreter/Tracing.hs
+++ src/Abstract/Interpreter/Tracing.hs
@@ -2,12 +2,13 @@
 module Abstract.Interpreter.Tracing where
 
 import Abstract.Configuration
+import Abstract.Environment
+import Abstract.Eval
 import Abstract.Interpreter
 import Abstract.Interpreter.Collecting
 import Abstract.Primitive
 import Abstract.Set
 import Abstract.Store
-import Abstract.Syntax
 import Abstract.Term
 import Abstract.Value
 
@@ -38,21 +39,38 @@ instance (Writer (g (Configuration l t v)) :< fs) => MonadTrace l t v g (Eff fs)
 
 -- Tracing and reachable state analyses
 
-evalTrace :: forall l v
-          .  (MonadAddress l (Eff (TraceInterpreter l (Term Prim) v)), MonadValue l v (Term Prim) (Eff (TraceInterpreter l (Term Prim) v)), MonadPrim v (Eff (TraceInterpreter l (Term Prim) v)), Semigroup (Cell l v))
-          => Eval (Term Prim) (TraceResult l (Term Prim) v [])
-evalTrace = run @(TraceInterpreter l (Term Prim) v) . fix (evTell @l @(Term Prim) @v @[] (ev @l))
+evalTrace :: forall l v s
+          . ( Ord v, Ord1 s, Ord1 (Cell l)
+            , MonadAddress l (Eff (TraceInterpreter l (Term s) v))
+            , MonadPrim v (Eff (TraceInterpreter l (Term s) v))
+            , Semigroup (Cell l v)
+            , Eval v (Eff (TraceInterpreter l (Term s) v)) s s
+            )
+          => Term s -> TraceResult l (Term s) v []
+evalTrace = run @(TraceInterpreter l (Term s) v) . fix (evTell @l @(Term s) @v @[] ev)
 
-evalReach :: forall l v a
-          .  (Ord a, Ord v, Ord l, Ord1 (Cell l), MonadAddress l (Eff (ReachableStateInterpreter l (Term Prim) v)), MonadValue l v (Term Prim) (Eff (ReachableStateInterpreter l (Term Prim) v)), MonadPrim v (Eff (ReachableStateInterpreter l (Term Prim) v)), Semigroup (Cell l v))
-          => Eval (Term Prim) (TraceResult l (Term Prim) v Set.Set)
-evalReach = run @(ReachableStateInterpreter l (Term Prim) v) . fix (evTell @l @(Term Prim) @v @Set.Set (ev @l))
+evalReach :: forall l v a s
+          . ( Ord a, Ord v, Ord l, Ord1 (Cell l), Ord1 s
+            , MonadAddress l (Eff (ReachableStateInterpreter l (Term s) v))
+            , MonadPrim v (Eff (ReachableStateInterpreter l (Term s) v))
+            , Semigroup (Cell l v)
+            , Eval v (Eff (ReachableStateInterpreter l (Term s) v)) s s
+            )
+          => Term s -> TraceResult l (Term s) v Set.Set
+evalReach = run @(ReachableStateInterpreter l (Term s) v) . fix (evTell @l @(Term s) @v @Set.Set ev)
 
 evTell :: forall l t v g m
-       .  (Ord l, IsList (g (Configuration l t v)), Item (g (Configuration l t v)) ~ Configuration l t v, MonadTrace l t v g m, MonadEnv l v m, MonadStore l v m, MonadGC l v m)
-       => (Eval t (m v) -> Eval t (m v))
-       -> Eval t (m v)
-       -> Eval t (m v)
+       . ( Ord l
+         , IsList (g (Configuration l t v))
+         , Item (g (Configuration l t v)) ~ Configuration l t v
+         , MonadTrace l t v g m
+         , MonadEnv l v m
+         , MonadStore l v m
+         , MonadGC l v m
+         )
+       => (Eval' t (m v) -> Eval' t (m v))
+       -> Eval' t (m v)
+       -> Eval' t (m v)
 evTell ev0 ev e = do
   env <- askEnv
   store <- getStore
